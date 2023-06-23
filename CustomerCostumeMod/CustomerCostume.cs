35ec9b981ae1f684340e0ef23426bc19340d9a58
MrSnowShark <chasek.martin96@gmail.com>
2023-06-22 21:09:25 -0500

    Initial commit

---
+++ CustomerCostumeMod/CustomerCostume.cs
@@ -0,0 +1,59 @@
+﻿using Kitchen;
+using KitchenData;
+using KitchenMods;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomerCostumeMod.System
+{
+    public class CustomerCostumeSystem : GameSystemBase, IModSystem
+    {
+        public List<PlayerCosmetic> Cosmetics = new List<PlayerCosmetic>();
+        //public List<PlayerCosmetic> Hats = new List<PlayerCosmetic>();
+        //public List<PlayerCosmetic> Outfits = new List<PlayerCosmetic>();
+        //public List<PlayerCosmetic> Others = new List<PlayerCosmetic>();
+        public List<CustomerType> Types = new List<CustomerType>();
+        public CustomerType CustomerType = new CustomerType();
+
+        protected override void Initialise()
+        {
+            base.Initialise();
+
+            Cosmetics = GameData.Main.Get<PlayerCosmetic>().ToList(); // Storing all Cosmetics into a List of PlayerCosmetics
+            Types = GameData.Main.Get<CustomerType>().ToList(); //
+
+            
+            foreach (var type in Types) // Iterating through CustomerTypes
+            {
+                if (type.name.ToString().Equals("Generic Customer")) // Checking for CusterType of "Generic Customer"
+                {
+                    CustomerType = type;
+                }
+            }
+
+            foreach (var cosmetic in Cosmetics) // Iterating through List of PlayerCosmetics
+            {
+                if (cosmetic.CosmeticType.ToString().Equals("Hat") && Main.PrefManager.Get<bool>(Main.COSTUME_HATS_ENABLED_ID)) // Checking for CosmeticType of "Hat"
+                {
+                    //Hats.Add(cosmetic); // Storing Hat Cosmetics to Hats List
+                    CustomerType.Cosmetics.Add(cosmetic); // Adding Hats to PlayerCosmetic List in "Generic Customer" CustomerType
+                }
+                else if (cosmetic.CosmeticType.ToString().Equals("Outfit") && Main.PrefManager.Get<bool>(Main.COSTUME_OUTFITS_ENABLED_ID)) // Checking for CosmeticType of "Outfit"
+                {
+                    //Outfits.Add(cosmetic); // Adding Outfits to PlayerCosmetic List in "Generic Customer" CustomerType
+                    CustomerType.Cosmetics.Add(cosmetic); // Storing Outfit Cosmetics to Outfits List
+                }
+                else // Checking for CosmeticType of anything that isn't a Hat or Outfit, aka Capes
+                {
+                    //Others.Add(cosmetic); // Storing Other Cosmetics to Others List
+                    //CustomerType.Cosmetics.Add(cosmetic); // Not Sure If I should add or not because they are disabled
+                }
+            }
+        }
+
+        protected override void OnUpdate()
+        {
+            
+        }
+    }   
+}
---
+++ CustomerCostumeMod/Main.cs
@@ -0,0 +1,80 @@
+﻿using KitchenData;
+using KitchenLib;
+using KitchenMods;
+using PreferenceSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Unity.Collections;
+
+namespace CustomerCostumeMod
+{
+    public class Main : BaseMod
+    {
+        public const string MOD_GUID = "MrSnowShark.PlateUp.CustomerCostumeMod";
+        public const string MOD_NAME = "CustomerCostumeMod";
+        public const string MOD_VERSION = "0.0.1";
+        public const string MOD_AUTHOR = "MrSnowShark";
+        public const string MOD_GAMEVERSION = ">=1.1.4";
+
+        public const string COSTUME_HATS_ENABLED_ID = "costumeHatsEnabled";
+        public const string COSTUME_OUTFITS_ENABLED_ID = "costumeOutfitsEnabled";
+
+        internal static PreferenceSystemManager PrefManager;
+
+        public Main() : base(MOD_GUID, MOD_NAME, MOD_AUTHOR, MOD_VERSION, MOD_GAMEVERSION, Assembly.GetExecutingAssembly()) { }
+
+        public virtual HashSet<string> RequiredModNames => new HashSet<string>()
+        {
+            "PreferenceSystem"
+        };
+
+        protected sealed override void OnPostActivate(Mod mod)
+        {
+            List<string> missingModNames = new List<string>();
+            IEnumerable<string> loadedModNames = ModPreload.Mods.Select(x => x.Name.ToLowerInvariant());
+            foreach (string name in RequiredModNames)
+            {
+                if (loadedModNames.Contains(name.ToLowerInvariant()))
+                    continue;
+                missingModNames.Add(name);
+            }
+            if (missingModNames.Count > 0)
+            {
+                throw new ModPackLoadException($"Error! Missing dependencies. {MOD_NAME} requires that you subscribe to {(String.Join(", ", missingModNames))}.");
+            }
+
+            PrefManager = new PreferenceSystemManager(MOD_GUID, MOD_NAME);
+            CreatePreferences();
+
+        }
+
+        private void CreatePreferences()
+        {
+            PrefManager
+                .AddLabel("Customer Costume Mod")
+                .AddSpacer()
+                .AddInfo("Changing \"Customer Costumes\" only takes effect upon game restart.")
+                .AddSpacer()
+                .AddLabel("Customer Hats")
+                .AddOption<bool>(
+                    COSTUME_HATS_ENABLED_ID,
+                        false,
+                        new bool[] { false, true },
+                        new string[] { "Disabled", "Enabled" })
+                .AddSpacer()
+                .AddLabel("Customer Outfits")
+                    .AddOption<bool>(
+                        COSTUME_OUTFITS_ENABLED_ID,
+                            false,
+                            new bool[] { false, true },
+                            new string[] { "Disabled", "Enabled" })
+                 .AddSpacer();
+
+            PrefManager.RegisterMenu(PreferenceSystemManager.MenuType.MainMenu);
+            PrefManager.RegisterMenu(PreferenceSystemManager.MenuType.PauseMenu);
+
+        }
+    }
+}
